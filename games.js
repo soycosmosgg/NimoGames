15344d3375fdf096bc7dea5a102ab929dcdfc757
soycosmosgg <tu-email-de-soycosmosgg@gmail.com>
2025-12-02 16:41:11 +0100

    updated

--- games.js
+++ games.js
@@ -100,6 +100,7 @@ const juegos = [
   { name: "Cookie Clicker", url: "https://webglmath.github.io/cookie-clicker/", img: "assets/game-images/cookie-clicker.webp", tags: ["clicker"] },
   { name: "Sprunki Clicker", url: "https://game.azgame.io/sprunki-clicker/", img: "assets/game-images/sprunki-clicker.webp", tags: ["clicker"] },
   { name: "Bottle Jump", url: "https://1games.io/game/bottle-jump/", img: "assets/game-images/bottle-jump.webp", tags: ["skill", "arcade"] },
+  { name: "Brain Test 1 Tricky Puzzles", url: "https://raw.githack.com/nimogamesdev/ng-source/main/game35/index.html", img: "assets/game-images/brain-test.webp", tags: ["puzzle"] },
   { name: "Brain Test 2 Tricky Stories", url: "https://raw.githack.com/nimogamesdev/ng-source/main/game11/index.html", img: "assets/game-images/brain-test-2-tricky-stories.webp", tags: ["puzzle"] },
   { name: "Brain Test 4 Tricky Friends", url: "https://raw.githack.com/nimogamesdev/ng-source-more/main/game34/index.html", img: "assets/game-images/brain-test-4-tricky-friends.webp", tags: ["puzzle"] },
   { name: "Cut the Rope", url: "https://games-site.github.io/projects/ctr-time-travel/index.html", img: "assets/game-images/cut-the-rope.webp", tags: ["puzzle"] },
